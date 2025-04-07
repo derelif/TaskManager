0fddfd7a626f852087922b961bc02d453281dfd5
igork <igorkogyt2003@gmail.com>
2025-04-07 16:05:35 +0300

    add POST, DELETE, PUT

--- src/main/java/com/naki/taskmanager/controller/TaskController.java
+++ src/main/java/com/naki/taskmanager/controller/TaskController.java
@@ -2,8 +2,8 @@ package com.naki.taskmanager.controller;
 
 import com.naki.taskmanager.entity.Task;
 import com.naki.taskmanager.repository.TaskRepository;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -24,5 +24,35 @@ public class TaskController {
     @GetMapping("/tasks")
     public List<Task> getTasks() {
         return taskRepository.findAll();
+    }
+
+    @PostMapping("/tasks")
+    public Task createTask(@RequestBody Task task) {
+        return taskRepository.save(task);
+    }
+
+    @DeleteMapping("/task/{id}")
+    public ResponseEntity<Void> deleteTask(@PathVariable Long id) {
+        if (taskRepository.existsById(id)) {
+            taskRepository.deleteById(id);
+            return ResponseEntity.noContent().build();
+        } else {
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    @PutMapping("/task/{id}")
+    public ResponseEntity<Task> updateTask(@PathVariable Long id, @RequestBody Task updateTask) {
+        return taskRepository.findById(id)
+                .map(task -> {
+                    task.setTitle(updateTask.getTitle());
+                    task.setDescription(updateTask.getDescription());
+                    task.setStatus(updateTask.getStatus());
+                    task.setDeadline(updateTask.getDeadline());
+                    task.setPriority(updateTask.getPriority());
+                    Task updatedTask = taskRepository.save(task);
+                    return ResponseEntity.ok(updatedTask);
+                })
+                .orElse(ResponseEntity.notFound().build());
     }
 }
--- src/main/java/com/naki/taskmanager/entity/Task.java
+++ src/main/java/com/naki/taskmanager/entity/Task.java
@@ -5,9 +5,13 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDate;
 
+@Setter
+@Getter
 @Entity
 public class Task {
 
